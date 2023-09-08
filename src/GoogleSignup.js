f132be31a110f5a4c9fb3cd665639f90ee28123e
mugheesansari888 <mugheesansari888@gmail.com>
2023-09-08 23:37:12 +0500

    google oAuth login done

--- src/GoogleSignup.js
+++ src/GoogleSignup.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import GoogleLogin from "react-google-login";
+import { GoogleLogin } from "@react-oauth/google";
 import {
   Card,
   CardContent,
@@ -10,31 +10,6 @@ import {
 } from "@mui/material";
 
 const GoogleSignup = () => {
-  const responseGoogle = async (response) => {
-    console.log("response: ", response);
-    // try {
-    //   // Send the Google OAuth token to your backend server
-    //   const { tokenId } = response;
-    //   const backendResponse = await fetch("http://localhost:3001/auth/google", {
-    //     method: "GET",
-    //     headers: {
-    //       "Content-Type": "application/json",
-    //     },
-    //     body: JSON.stringify({ tokenId }),
-    //   });
-
-    //   // console.log("backendResponse : ", backendResponse);
-
-    //   if (backendResponse.ok) {
-    //     console.log("Successfully authenticated with Google");
-    //   } else {
-    //     console.error("Authentication with Google failed");
-    //   }
-    // } catch (error) {
-    //   console.error("Error while authenticating with Google:", error);
-    // }
-  };
-
   return (
     <Container
       style={{
@@ -56,13 +31,12 @@ const GoogleSignup = () => {
             Sign Up with Google
           </Typography>
           <GoogleLogin
-            clientId="497095083295-um2a987d5042tfn7okgome1rcei87crj.apps.googleusercontent.com"
-            // clientId="11013940099-b1s7u09t7i4aecd3c8ddj1l4f3j32ftv.apps.googleusercontent.com"
-            buttonText="Sign Up with Google"
-            onSuccess={responseGoogle}
-            onFailure={responseGoogle}
-            cookiePolicy={"single_host_origin"}
-            redirectUri="http://localhost:3000/auth/google/callback"
+            onSuccess={(credentialResponse) => {
+              console.log(credentialResponse);
+            }}
+            onError={() => {
+              console.log("Login Failed");
+            }}
           />
         </CardContent>
       </Card>
@@ -71,73 +45,3 @@ const GoogleSignup = () => {
 };
 
 export default GoogleSignup;
-
-// import React, { useEffect } from "react";
-// import { GoogleLogin } from "react-google-login";
-// import { gapi } from "gapi-script";
-
-// function GoogleSignup() {
-//   useEffect(() => {
-//     function start() {
-//       gapi.client.init({
-//         clientId:
-//           "11013940099-b1s7u09t7i4aecd3c8ddj1l4f3j32ftv.apps.googleusercontent.com",
-//         scope: "email",
-//       });
-//     }
-
-//     gapi.load("client:auth2", start);
-//   }, []);
-
-//   // **you can access the token like this**
-//   // const accessToken = gapi.auth.getToken().access_token;
-//   // console.log(accessToken);
-
-//   // const onSuccess = async (response) => {
-//   //   console.log("response: ", response);
-//   //   // const { tokenId } = response;
-//   //   // try {
-//   //   //   // Send the Google OAuth token to your backend server
-//   //   //   // const { tokenId } = response;
-//   //   //   const backendResponse = await fetch("http://localhost:3001/auth/google", {
-//   //   //     method: "GET",
-//   //   //     headers: {
-//   //   //       "Content-Type": "application/json",
-//   //   //     },
-//   //   //     body: JSON.stringify({ accessToken }),
-//   //   //   });
-
-//   //   //   // console.log("backendResponse : ", backendResponse);
-
-//   //   //   if (backendResponse.ok) {
-//   //   //     console.log("Successfully authenticated with Google");
-//   //   //   } else {
-//   //   //     console.error("Authentication with Google failed");
-//   //   //   }
-//   //   // } catch (error) {
-//   //   //   console.error("Error while authenticating with Google:", error);
-//   //   // }
-//   // };
-
-//   const onSuccess = (response) => {
-//     console.log("SUCCESS", response);
-//   };
-//   const onFailure = (response) => {
-//     console.log("FAILED", response);
-//   };
-
-//   return (
-//     <div>
-//       <GoogleLogin
-//         clientId="11013940099-b1s7u09t7i4aecd3c8ddj1l4f3j32ftv.apps.googleusercontent.com"
-//         onSuccess={onSuccess}
-//         onFailure={onFailure}
-//         accessType="offline" // Request offline access for refresh token
-//         cookiePolicy={"single_host_origin"}
-//         redirectUri="http://localhost:3000"
-//       />
-//     </div>
-//   );
-// }
-
-// export default GoogleSignup;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <GoogleOAuthProvider clientId="497095083295-fgvetge38babrt7t53dpge4vt58vn7kv.apps.googleusercontent.com">
+      <App />
+    </GoogleOAuthProvider>
   </React.StrictMode>
 );
