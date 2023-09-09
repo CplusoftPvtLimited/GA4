6e7fe3c14fa5fca806e6b12bb1db85b324f24059
abdullahSaffy <khansaffy1@gmail.com>
2023-09-10 00:35:17 +0500

    update the end points

--- server.js
+++ server.js
@@ -18,11 +18,8 @@ app.use(
 // Configure the OAuth 2.0 client
 const oauth2Client = new google.auth.OAuth2(
   "497095083295-um2a987d5042tfn7okgome1rcei87crj.apps.googleusercontent.com",
-  // "11013940099-b1s7u09t7i4aecd3c8ddj1l4f3j32ftv.apps.googleusercontent.com",
-  // "GGOCSPX-LlsBtGnqFfw5Xbizi7xnfW2cNYDv",
   "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",
   "http://localhost:3000"
-  // "https://www.google.com/"
 );
 
 // Define the Google Analytics API version and scopes
@@ -30,17 +27,19 @@ const analytics = google.analytics("v3");
 const scopes = ["https://www.googleapis.com/auth/analytics.readonly"];
 
 // Define the route for initiating the OAuth flow
-app.get("/auth/google", (req, res) => {
+app.post("/auth/google", (req, res) => {
   const url = oauth2Client.generateAuthUrl({
     access_type: "offline",
     scope: scopes,
   });
+  console.log("url: ", url);
   res.redirect(url);
 });
 
 // Handle the OAuth callback
 app.get("/auth/google/callback", async (req, res) => {
   const code = req.query.code;
+  console.log("code: ", code);
   const { tokens } = await oauth2Client.getToken(code);
   oauth2Client.setCredentials(tokens);
   req.session.tokens = tokens; // Store the tokens in the session
@@ -49,10 +48,11 @@ app.get("/auth/google/callback", async (req, res) => {
 
 // Define a route to fetch Google Analytics data
 app.get("/analytics", async (req, res) => {
+  console.log("analytics");
   try {
     const result = await analytics.data.ga.get({
       auth: oauth2Client,
-      ids: "260508773", // Replace with your Google Analytics view ID
+      ids: "358239987", // Replace with your Google Analytics view ID
       "start-date": "7daysAgo",
       "end-date": "today",
       metrics: "ga:sessions,ga:pageviews",
